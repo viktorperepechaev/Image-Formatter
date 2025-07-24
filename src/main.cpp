13c3281c7496722718867510b7e27f6a67c604a0
Viktor Perepechaev <viktor2309msk@gmail.com>
2025-07-24 10:04:32 +0300

    Added Class Image

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,64 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+class Image {
+ public:
+  Image() = default;
+  Image(const char* name);
+  
+  ~Image();
+
+  void Darken(const char percent);
+  
+  void CreateOutputImage(const char* new_image_name) const;
+
+ private:
+  unsigned char* data_ = nullptr;
+  int width_ = 0;
+  int height_ = 0;
+  int number_of_channels_ = 0;
+};
+
+Image::Image(const char* name) {
+  data_ = stbi_load(name, &width_, &height_, &number_of_channels_, 0);
+
+  if (data_ == NULL) {
+    throw std::runtime_error("Не смог открыть изначальный файл" + std::string(name) + "\nПричина: " + stbi_failure_reason());
+  }
+
+  std::cout << "Успешно открыл изначальный файл!" << std::endl;
+}
+
+Image::~Image() {
+  stbi_image_free(data_);
+}
+
+void Image::Darken(const char percent) {
+  for (int current_line = 0; current_line < height_; ++current_line) {
+    for (int current_column = 0; current_column < width_; ++current_column) {
+      unsigned char* current_pixel = data_ + (width_ * current_line + current_column) * number_of_channels_;
+
+      unsigned char& red = current_pixel[0];
+      unsigned char& green = current_pixel[1];
+      unsigned char& blue = current_pixel[2];
+
+      red = std::lround(red * 0.01 * percent);
+      green = std::lround(green * 0.01 * percent);
+      blue = std::lround(blue * 0.01 * percent);
+    }
+  }
+}
+
+void Image::CreateOutputImage(const char* new_image_name) const {
+  int stride = width_ * number_of_channels_;
+  
+  if (!stbi_write_png(new_image_name, width_, height_, number_of_channels_, data_, stride)) { // Может стоит здесь как-то память очистить???
+    throw std::runtime_error("Не удалось сохранить файл " + std::string(new_image_name));
+  }
+
+  std::cout << "Результат сохранен в файл " << std::string(new_image_name) << "!" << std::endl;
+}
+
 int main(int argc, char** argv) {
   if (argc != 2) { 
     throw std::runtime_error("Неправильный список аргументов. Он должен выглядеть так: [<name_of_the_picture>]");
@@ -26,32 +84,10 @@ int main(int argc, char** argv) {
     throw std::runtime_error("Не смог открыть изначальный файл " + std::string(argv[1]) + "\nПричина: " + stbi_failure_reason());
   }
   
-  std::cout << "Успешно открыл изначальный файл!" << std::endl;
+  Image image(argv[1]);
 
-  for (int current_line = 0; current_line < image_height; ++current_line) {
-    for (int current_column = 0; current_column < image_width; ++current_column) {
-      unsigned char* current_pixel = data + (image_width * current_line + current_column) * image_number_of_channels;
-
-      unsigned char& red = current_pixel[0];
-      unsigned char& green = current_pixel[1];
-      unsigned char& blue = current_pixel[2];
-
-      red /= 2;
-      green /= 2;
-      blue /= 2;
-    }
-  }
-
-  int stride = image_width * image_number_of_channels;
+  image.Darken(10);
+  image.CreateOutputImage("sigma.png");
   
-  if (!stbi_write_png("result.png", image_width, image_height, image_number_of_channels, data, stride)) {
-    stbi_image_free(data);
-
-    throw std::runtime_error("Не удалось сохранить файл result.png");
-  }
-
-  std::cout << "Результат сохранен в файл " << "result.png" << "." << std::endl;
-  
-  stbi_image_free(data);
   return 0;
 }
