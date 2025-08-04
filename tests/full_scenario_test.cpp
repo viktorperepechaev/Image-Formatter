b7e775c43b595fc3ac83775637866468831b13e3
Viktor Perepechaev <viktor2309msk@gmail.com>
2025-08-04 13:23:05 +0300

    Added 1 test

--- tests/full_scenario_test.cpp
+++ tests/full_scenario_test.cpp
@@ -1,8 +1,13 @@
 #include <gtest/gtest.h>
 
+#include "../include/imageformatter/Image.hpp"
+
 TEST(TrivialTest, Trivial) { EXPECT_TRUE(2 == 2); }
 
-// Точка входа для GTest
+TEST(ImageTest, ValidLoading) {
+  EXPECT_NO_THROW(Image("Cute.jpg"));
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
