02e4d76e5473129d1f6eefc270bfee0a0a28c9af
Viktor Perepechaev <viktor2309msk@gmail.com>
2025-08-04 10:14:28 +0300

    Setup coc.nvim for gtest

---
+++ tests/CMakeFiles/ImageFormatter_Tests.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ImageFormatter_Tests.
+# This may be replaced when dependencies are built.
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/viktor/MIPT/CPP/Playground/Image-To-Ascii-Converter/tests
+# Build directory: /Users/viktor/MIPT/CPP/Playground/Image-To-Ascii-Converter/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/viktor/MIPT/CPP/Playground/Image-To-Ascii-Converter/tests/ImageFormatter_Tests[1]_include.cmake")
+subdirs("../_deps/googletest-build")
